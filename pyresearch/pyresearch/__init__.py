c400c6663939381f4628ae8a447c497817c6cc72
Steve <estarcia97@gmail.com>
2026-05-14 16:49:56 -0500

    removed unneeded files

--- pyresearch/pyresearch/__init__.py
+++
@@ -1,3 +0,0 @@
-from .acs import collect_acs_data
-from .rucc import collect_rucc_data
-from .ice import merge_ice_data
--- pyresearch/pyresearch/data_classes/utils/model_config.py
+++
@@ -1,9 +0,0 @@
-from pydantic import ConfigDict
-
-MODEL_CONFIG = ConfigDict(
-    str_strip_whitespace=True,
-    populate_by_name=True,
-    validate_assignment=True,
-    arbitrary_types_allowed=True,
-    validate_return=True,
-)
