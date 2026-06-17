d9cf9fc3021e99b98757c6ad02c24d319f4a812f
Steve <estarcia97@gmail.com>
2026-06-16 20:09:10 -0500

    refactor: update notebooks and exports to use CensusDataLoader

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .ruca import collect_ruca_data
-from .data_loader import CensusConfig, BaseCensusDataLoader
+from .data_loader import CensusDataLoader
 
-__all__ = ["collect_ruca_data", "CensusConfig", "BaseCensusDataLoader"]
+__all__ = ["collect_ruca_data", "CensusDataLoader"]
