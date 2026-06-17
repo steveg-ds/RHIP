81b62fe81823878e744a9bac3959429bc5e038c3
Steve <estarcia97@gmail.com>
2026-06-16 19:55:58 -0500

    refactor: address data loader review feedback

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,27 +1,27 @@
 import pandas as pd
 from pydantic import BaseModel, Field
 from typing import List, Optional
+import pytidycensus as tc
 
 class CensusConfig(BaseModel):
     year: int = Field(default=2024, ge=2010, le=2026)
-    states: List[str] = Field(default_factory=list)
+    states: Optional[List[str]] = None
     api_key: Optional[str] = None
 
 class BaseCensusDataLoader:
     def __init__(self, config: CensusConfig):
         self.config = config
-        self._raw_data: Optional[pd.DataFrame] = None
+        self._cache = {}  # dict mapping variables tuple to DataFrame
         if self.config.api_key:
-            import pytidycensus as tc
             tc.set_census_api_key(self.config.api_key)
 
     def fetch(self, variables: List[str]) -> pd.DataFrame:
-        if self._raw_data is None:
-            import pytidycensus as tc
-            self._raw_data = tc.get_acs(
+        var_tuple = tuple(variables)
+        if var_tuple not in self._cache:
+            self._cache[var_tuple] = tc.get_acs(
                 geography="tract",
                 variables=variables,
                 state=self.config.states,
                 year=self.config.year
             )
-        return self._raw_data
+        return self._cache[var_tuple]
